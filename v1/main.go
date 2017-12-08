e3af76a4e542afac31dbe05b677bace404db31d0
Giancarlo Rubio <gianrubio@gmail.com>
2017-12-08 10:35:08 +0100

    V2

--- v1/main.go
+++ v1/main.go
@@ -12,8 +12,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		userAgent := r.Header.Get("User-Agent")
 		log.Printf("%s - [%s %s %s] 200 [%s]", r.RemoteAddr, r.Method, r.URL.Path, r.Proto, userAgent)
-		w.Header().Set("Server", "Hello/v1")
-		fmt.Fprintf(w, "Hello, World!\nVersion: 1")
+		w.Header().Set("Server", "Hello/v2")
+		fmt.Fprintf(w, "Hello, World!\nVersion: 2")
 	})
 	http.Handle("/metrics", promhttp.Handler())
 	log.Println("Starting Hello service...")
